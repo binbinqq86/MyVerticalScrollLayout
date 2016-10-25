036e45bac36fb5d6711c9e26dfa7d01d65422556
tianbin <tianbin@kuker.tv>
2016-10-25 16:53:28 +0800

    修复bug

--- app/src/main/java/com/binbin/verticalscrolllayout/MainActivity.java
+++ app/src/main/java/com/binbin/verticalscrolllayout/MainActivity.java
@@ -8,7 +8,9 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.binbin.verticalscrolllayout.VerticalScrollLayout;
 
@@ -24,16 +26,47 @@ public class MainActivity extends Activity {
         for (int i = 0; i < 5; i++) {
             View view=LayoutInflater.from(this).inflate(R.layout.view, null);
             ((TextView)view.findViewById(R.id.tv)).setText("---"+i);
+            ((Button)view.findViewById(R.id.bt)).setText("==="+i);
             if(i==0){
                 view.setBackgroundColor(Color.RED);
+                ((Button)view.findViewById(R.id.bt)).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(MainActivity.this,"haha0",Toast.LENGTH_SHORT).show();
+                    }
+                });
             }else if(i==1){
                 view.setBackgroundColor(Color.GREEN);
+                ((Button)view.findViewById(R.id.bt)).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(MainActivity.this,"haha1",Toast.LENGTH_SHORT).show();
+                    }
+                });
             }else if(i==2){
                 view.setBackgroundColor(Color.BLUE);
+                ((Button)view.findViewById(R.id.bt)).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(MainActivity.this,"haha22",Toast.LENGTH_SHORT).show();
+                    }
+                });
             }else if(i==3){
                 view.setBackgroundColor(Color.CYAN);
+                ((Button)view.findViewById(R.id.bt)).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(MainActivity.this,"haha333",Toast.LENGTH_SHORT).show();
+                    }
+                });
             }else if(i==4){
                 view.setBackgroundColor(Color.YELLOW);
+                ((Button)view.findViewById(R.id.bt)).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(MainActivity.this,"haha444",Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
             mViews.add(view);
         }
--- app/src/main/java/com/binbin/verticalscrolllayout/VerticalScrollLayout.java
+++ app/src/main/java/com/binbin/verticalscrolllayout/VerticalScrollLayout.java
@@ -15,6 +15,7 @@ import android.view.MotionEvent;
 import android.view.VelocityTracker;
 import android.view.View;
 import android.view.ViewConfiguration;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
@@ -26,13 +27,13 @@ import android.widget.Scroller;
  */
 
 @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-public class VerticalScrollLayout extends LinearLayout {
+public class VerticalScrollLayout extends ViewGroup {
     /** 用于滑动的类*/
     private Scroller mScroller;
     /** 用来跟踪触摸速度的类*/
     private VelocityTracker mVelocityTracker;
     /** 最小滑动的速度*/
-    private static final int SNAP_VELOCITY = 400;
+    private static final int SNAP_VELOCITY = 600;
     /**最小滑动距离，超过了，才认为开始滑动  */
     private int mTouchSlop = 0 ;
     /**上次触摸的X坐标*/
@@ -75,7 +76,6 @@ public class VerticalScrollLayout extends LinearLayout {
         this.mContext=context;
         mScroller = new Scroller(context);
         mTouchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
-        setOrientation(VERTICAL);
     }
 	
     public void addViews(List<View> mViews){
@@ -83,6 +83,75 @@ public class VerticalScrollLayout extends LinearLayout {
     	for(int i=0;i<mViews.size();i++){
     		addView(mViews.get(i),i,new LayoutParams(-1,-1));
     	}
+    }
+
+    private int maxWidth,maxHeight;
+    /**
+     * 计算所有ChildView的宽度和高度 然后根据ChildView的计算结果，设置ViewGroup自己的宽和高
+     * Exactly：width代表的是精确的尺寸
+     AT_MOST：width代表的是最大可获得的空间
+     MATCH_PARENT(FILL_PARENT)对应于EXACTLY，WRAP_CONTENT对应于AT_MOST
+     其他情况(有具体值的)也对应于EXACTLY
+     */
+    @SuppressLint("NewApi")
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        //当我们需要重写onMeasure时，记得要调用setMeasuredDimension来设置自身的mMeasuredWidth和mMeasuredHeight，否则，就会抛出异常
+        /**
+         * 获得此ViewGroup上级容器为其推荐的宽和高，以及计算模式
+         */
+//      final int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+//      final int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+//
+        int measuredWidth = MeasureSpec.getSize(widthMeasureSpec);
+        int measuredHeight = MeasureSpec.getSize(heightMeasureSpec);
+
+        /**
+         * 根据childView计算的出的宽和高，计算容器的宽和高，主要用于容器是warp_content时
+         */
+//        for (int i = 0,count = getChildCount(); i < count; i++) {
+//            View childView = getChildAt(i);
+//            //获取每个子view的自己高度宽度，取最大的就是viewGroup的大小
+//            measureChild(childView, widthMeasureSpec, heightMeasureSpec);
+//            maxWidth = Math.max(maxWidth,childView.getMeasuredWidth());
+//            maxHeight = Math.max(maxHeight,childView.getMeasuredHeight());
+//        }
+        //为ViewGroup设置宽高
+        setMeasuredDimension(measuredWidth,measuredHeight);
+
+        // 计算出所有的childView的宽和高---可用
+        measureChildren(widthMeasureSpec, heightMeasureSpec);
+
+        /**
+         * 设置所有的childView的宽和高，此处如果不设置，会造成多个子view的情况下，有的子view设置成match_parent但是不能充满父控件的问题
+         */
+        //首先判断params.width的值是多少，有三种情况。
+        //如果是大于零的话，及传递的就是一个具体的值，那么，构造MeasupreSpec的时候可以直接用EXACTLY。
+        //如果为-1的话，就是MatchParent的情况，那么，获得父View的宽度，再用EXACTLY来构造MeasureSpec。
+        //如果为-2的话，就是wrapContent的情况，那么，构造MeasureSpec的话直接用一个负数就可以了。
+//        for (int i = 0,count = getChildCount(); i < count; i++) {
+//            View childView = getChildAt(i);
+//            int widthSpec = 0;
+//            int heightSpec = 0;
+//            ViewGroup.LayoutParams params = childView.getLayoutParams();
+//            if(params.width > 0){
+//                widthSpec = MeasureSpec.makeMeasureSpec(params.width, MeasureSpec.EXACTLY);
+//            }else if (params.width == -1) {
+//                widthSpec = MeasureSpec.makeMeasureSpec(maxWidth, MeasureSpec.EXACTLY);
+//            } else if (params.width == -2) {
+//                widthSpec = MeasureSpec.makeMeasureSpec(maxWidth, MeasureSpec.AT_MOST);
+//            }
+//
+//            if(params.height > 0){
+//                heightSpec = MeasureSpec.makeMeasureSpec(params.height, MeasureSpec.EXACTLY);
+//            }else if (params.height == -1) {
+//                heightSpec = MeasureSpec.makeMeasureSpec(maxHeight, MeasureSpec.EXACTLY);
+//            } else if (params.height == -2) {
+//                heightSpec = MeasureSpec.makeMeasureSpec(maxHeight, MeasureSpec.AT_MOST);
+//            }
+//            childView.measure(widthSpec, heightSpec);
+//        }
     }
 
     /**
@@ -103,17 +172,27 @@ public class VerticalScrollLayout extends LinearLayout {
     }
 
     /**
-     * 滚动，startX, startY为开始滚动的位置（包括移出屏幕的地方的坐标，一般以滑动的距离为起点）
-     * dx,dy为滚动的偏移量（正值代表向左，向上移动，负值代表向右，向下移动）
-     * duration为完成滚动的时间
-     * mScroller.startScroll(int startX, int startY, int dx, int dy)
-     * 使用默认完成时间250ms
-     * mScroller.startScroll(int startX, int startY, int dx, int dy, int duration)
+     * ViewGroup在分发绘制自己的孩子的时候，会对其子View调用computeScroll()方法
      */
     @SuppressLint("NewApi")
 	@Override
-    public boolean dispatchTouchEvent(MotionEvent ev) {
+    public void computeScroll() {
         // TODO Auto-generated method stub
+        if (mScroller.computeScrollOffset()) {
+            scrollTo(mScroller.getCurrX(), mScroller.getCurrY());
+            postInvalidate();
+        }else{//滑动结束后
+            if(isFinished){
+                isFinished=false;
+                Log.e("tianbin","tianbincomplete-------------"+getScrollY());
+                Log.e("tianbin","tianbincomplete3333-------------"+getScrollY());
+            }
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+
         if (mVelocityTracker == null) {
             // 使用obtain方法得到VelocityTracker的一个对象
             mVelocityTracker = VelocityTracker.obtain();
@@ -128,13 +207,9 @@ public class VerticalScrollLayout extends LinearLayout {
                 mLastY=ev.getY();
                 break;
             case MotionEvent.ACTION_MOVE:
-                mVelocityTracker.computeCurrentVelocity(1000);
-                // 获得当前的速度
-                velocityY = (int) mVelocityTracker.getYVelocity();
                 // 计算当前的速度
-                if(Math.abs(velocityY)>SNAP_VELOCITY||
-                		Math.abs(mLastY-ev.getY())>mTouchSlop&&
-                		Math.abs(mLastX-ev.getX())<mTouchSlop){
+                if(Math.abs(mLastY-ev.getY())>mTouchSlop&&
+                                Math.abs(mLastX-ev.getX())<mTouchSlop){
                     isSliding=true;
 
                     int deltaY = (int) (mLastY - ev.getY());
@@ -153,54 +228,58 @@ public class VerticalScrollLayout extends LinearLayout {
                 int delta=0;
                 int scrollY=getScrollY()%CONTENT_HEIGHT;
 
+                mVelocityTracker.computeCurrentVelocity(100);
+                // 获得当前的速度
+                velocityY = (int) mVelocityTracker.getYVelocity();
+
                 Log.e("tianbin",getScrollY()+"tianbin@@@@@uppppppp"+"###"+totalScrollY+"###"+scrollY);
-                
+
                 //当前为第一屏或者滑动距离不够的时候，向下滑动自动向上回弹
                 if((Math.abs(totalScrollY)<ratio||
-                		currentScreen==0)&&
-                		totalScrollY<0){
-                	if(currentScreen==0){
-                		delta=getScrollY();
-                	}else{
-                		//非第一屏，getScrollY()小于屏幕高度，但为正值
-                		delta=currentScreen*CONTENT_HEIGHT-getScrollY();
-                	}
+                        currentScreen==0)&&
+                        totalScrollY<0){
+                    if(currentScreen==0){
+                        delta=getScrollY();
+                    }else{
+                        //非第一屏，getScrollY()小于屏幕高度，但为正值
+                        delta=currentScreen*CONTENT_HEIGHT-getScrollY();
+                    }
                     mScroller.startScroll(0,getScrollY(), 0,Math.abs(delta));
                     invalidate();
                 }
                 //当前为最后一屏或者滑动距离不够的时候，向上滑动自动向下回弹
                 if((Math.abs(totalScrollY)<ratio||
-                		currentScreen==mViews.size()-1)&&
-                		totalScrollY>0){
-                	delta=getScrollY()-currentScreen*CONTENT_HEIGHT;
+                        currentScreen==mViews.size()-1)&&
+                        totalScrollY>0){
+                    delta=getScrollY()-currentScreen*CONTENT_HEIGHT;
                     mScroller.startScroll(0,getScrollY(), 0,-Math.abs(delta));
                     invalidate();
                 }
-                
-                
+
+
                 //当前屏是中间屏，且滑动距离或者滑动速度满足需求，则自动向上或者向下滑一屏
-                if(/**Math.abs(velocityY)>SNAP_VELOCITY||*/
-                		Math.abs(totalScrollY)>=ratio){
-                	if(currentScreen!=mViews.size()-1&&totalScrollY>0){
-                		//向上滑动，自动切下一屏
-                		delta=(currentScreen+1)*CONTENT_HEIGHT-getScrollY();
+                if(Math.abs(velocityY)>SNAP_VELOCITY||
+                        Math.abs(totalScrollY)>=ratio){
+                    if(currentScreen!=mViews.size()-1&&totalScrollY>0){
+                        //向上滑动，自动切下一屏
+                        delta=(currentScreen+1)*CONTENT_HEIGHT-getScrollY();
                         mScroller.startScroll(0,getScrollY(), 0,Math.abs(delta));
                         invalidate();
                         currentScreen++;
                         isFinished=true;//切屏才算结束，回弹不算
-                	}
-                	if(currentScreen!=0&&totalScrollY<0){
-                		delta=getScrollY()-(currentScreen-1)*CONTENT_HEIGHT;
-                    	mScroller.startScroll(0,getScrollY(), 0,-Math.abs(delta));
+                    }
+                    if(currentScreen!=0&&totalScrollY<0){
+                        delta=getScrollY()-(currentScreen-1)*CONTENT_HEIGHT;
+                        mScroller.startScroll(0,getScrollY(), 0,-Math.abs(delta));
                         invalidate();
-                    	currentScreen--;
+                        currentScreen--;
                         isFinished=true;//切屏才算结束，回弹不算
-                	}
+                    }
                 }
-                
+
                 totalScrollY=0;
                 if (mVelocityTracker != null) {
-                	mVelocityTracker.clear();
+                    mVelocityTracker.clear();
                     mVelocityTracker.recycle();
                     mVelocityTracker = null;
                 }
@@ -209,22 +288,21 @@ public class VerticalScrollLayout extends LinearLayout {
         return true;
     }
 
-    /**
-     * ViewGroup在分发绘制自己的孩子的时候，会对其子View调用computeScroll()方法
-     */
-    @SuppressLint("NewApi")
-	@Override
-    public void computeScroll() {
-        // TODO Auto-generated method stub
-        if (mScroller.computeScrollOffset()) {
-            scrollTo(mScroller.getCurrX(), mScroller.getCurrY());
-            postInvalidate();
-        }else{//滑动结束后
-            if(isFinished){
-                isFinished=false;
-                Log.e("tianbin","tianbincomplete-------------"+getScrollY());
-                Log.e("tianbin","tianbincomplete3333-------------"+getScrollY());
-            }
+    @Override
+    public boolean onInterceptHoverEvent(MotionEvent ev) {
+        switch (ev.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                mLastX=ev.getRawX();
+                mLastY=ev.getRawY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if((Math.abs(mLastY-ev.getRawY())>mTouchSlop)&&
+                        (Math.abs(mLastX-ev.getRawX())<mTouchSlop)){
+                    //自己来处理滑动
+                    return true;
+                }
+                break;
         }
+        return super.onInterceptHoverEvent(ev);
     }
 }
